71ad0d5d3bf891a8029ba68658f2e1df830d0f38
thomas-spinec <thomas.spinec@laplateforme.io>
2023-01-10 16:00:37 +0100

    ajout du footer dans la page classement, ajout d'un margin bottom au dessous des formulaire pour plus de clarté

--- classement.php
+++ classement.php
@@ -32,4 +32,12 @@
                     }
                     $player->getBestScores();
                 ?>
+            </div>
         </div>
+    </div>
+</main>
+
+<!-- footer des pages -->
+<?php
+    require 'include/footer.php';
+?>
